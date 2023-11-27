a77eb2446ea5c8926c27974e6342ac010b6334e5
BorisNeykov1202 <bobineikov@gmail.com>
2023-11-27 15:21:47 +0000

    code refactor, WebClient instead of Rest Template

--- RidePalApplication/src/main/java/com/example/ridepalapplication/helpers/AuthenticationHelper.java
+++ RidePalApplication/src/main/java/com/example/ridepalapplication/helpers/AuthenticationHelper.java
@@ -1,14 +1,11 @@
 package com.example.ridepalapplication.helpers;
 
-import com.example.ridepalapplication.exceptions.AuthorizationException;
-import com.example.ridepalapplication.exceptions.EntityNotFoundException;
+
 import com.example.ridepalapplication.models.User;
 import com.example.ridepalapplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
